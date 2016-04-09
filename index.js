7d627c3dc4a56ceb134f9adcebae2be38a2dbd4b
Eric Lifka <eric.lifka@gmail.com>
2016-04-09 09:33:07 -0400

    provide an index.js to serve as repo main

---
+++ index.js
@@ -0,0 +1,22 @@
+DefineModule('pxlr-fonts', function (require) {
+    var arcade = require('pxlr-fonts/arcade');
+    var arcadeSmall = require('pxlr-fonts/arcade-small');
+    var phoenix = require('pxlr-fonts/phoenix');
+
+    return {
+        arcade: arcade,
+        arcadeSmall: arcadeSmall,
+        phoenix: phoenix
+    };
+});
+
+/* provide namespace backwards compatibility for v1 */
+DefineModule('fonts/arcade', function (require) {
+    return require('pxlr-fonts/arcade');
+});
+DefineModule('fonts/arcade-small', function (require) {
+    return require('pxlr-fonts/arcade-small');
+});
+DefineModule('fonts/phoenix', function (require) {
+    return require('pxlr-fonts/phoenix');
+});
