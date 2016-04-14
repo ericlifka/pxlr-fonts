635ee32b4fc2bf63864259a10a7c239b38c5c49d
Eric Lifka <eric.lifka@gmail.com>
2016-04-13 23:14:48 -0400

    add github demo page task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-const gulp = require('gulp');
 const concat = require('gulp-concat');
+const filter = require('gulp-filter');
+const gulp = require('gulp');
+const ghPages = require('gulp-gh-pages');
 const wrapper = require('gulp-wrapper');
+const htmlbuild = require('gulp-htmlbuild');
 
 gulp.task('default', function () {
     return gulp.src([
@@ -13,4 +16,30 @@ gulp.task('default', function () {
             footer: '/* end:pxlr-fonts */\n}());\n'
         }))
         .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('deploy-demo', function () {
+    var jsFilter = filter('**/*.js', {restore: true});
+    var htmlFilter = filter('**/*.html', {restore: true});
+
+    return gulp.src([
+            'bower_components/simple-web-modules/index.js',
+            'bower_components/pxlr-core/dist/index.js',
+            'bower_components/pxlr-gl/dist/index.js',
+            'fonts/**/*',
+            'demo/**/*',
+            'index.js'
+        ])
+        .pipe(jsFilter)
+        .pipe(concat({ path: 'demo.js' }))
+        .pipe(jsFilter.restore)
+        .pipe(htmlFilter)
+        .pipe(htmlbuild({
+            js: htmlbuild.preprocess.js(function (block) {
+                block.write('demo.js');
+                block.end();
+            })
+        }))
+        .pipe(htmlFilter.restore)
+        .pipe(ghPages());
 });
