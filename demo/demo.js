27330a4d026eaad0afddee9e21e843f58a8b501e
Eric Lifka <eric.lifka@gmail.com>
2016-04-13 22:30:02 -0400

    make a letter the super low level way

--- demo/demo.js
+++ demo/demo.js
@@ -1,11 +1,15 @@
 DefineModule('main', function (require) {
     var CanvasRenderer = require('pxlr/gl/canvas');
+    var ArcadeSmall = require('pxlr/fonts/arcade-small');
 
     var renderer = new CanvasRenderer({ width: 200, height: 150 });
     renderer.setFillColor("#FFFFFF");
 
     var frame = renderer.newRenderFrame();
     frame.clear();
+
+    ArcadeSmall.A.applyColor("#000000");
+    ArcadeSmall.A.renderToFrame(frame, 0, 0);
 
     renderer.renderFrame();
 });
