2d2b1f386060f5852ef8c8320ef382da5ce6c760
Eric Lifka <eric.lifka@gmail.com>
2016-04-19 09:47:31 -0400

    move the lower case letters down to establish a baseline

--- fonts/elian.js
+++ fonts/elian.js
@@ -3,6 +3,7 @@ DefineModule('pxlr/fonts/elian', function (require) {
 
     var w = "white";
     var n = null;
+    var lowerCaseOffset = { x: 0, y: 2 };
 
     return {
         meta: {
@@ -15,49 +16,49 @@ DefineModule('pxlr/fonts/elian', function (require) {
             [w, w, w],
             [n, n, w],
             [n, n, w]
-        ]).invertY().rotateRight(),
+        ]).invertY().rotateRight().setPermanentOffset(lowerCaseOffset),
         B: new Sprite([
             [w, w, w],
             [n, n, w],
             [w, w, w]
-        ]).invertY().rotateRight(),
+        ]).invertY().rotateRight().setPermanentOffset(lowerCaseOffset),
         C: new Sprite([
             [n, n, w],
             [n, n, w],
             [w, w, w]
-        ]).invertY().rotateRight(),
+        ]).invertY().rotateRight().setPermanentOffset(lowerCaseOffset),
 
         D: new Sprite([
             [w, w, w],
             [w, n, w],
             [w, n, w]
-        ]).invertY().rotateRight(),
+        ]).invertY().rotateRight().setPermanentOffset(lowerCaseOffset),
         E: new Sprite([
             [w, w, w],
             [w, n, w],
             [w, w, w]
-        ]).invertY().rotateRight(),
+        ]).invertY().rotateRight().setPermanentOffset(lowerCaseOffset),
         F: new Sprite([
             [w, n, w],
             [w, n, w],
             [w, w, w]
-        ]).invertY().rotateRight(),
+        ]).invertY().rotateRight().setPermanentOffset(lowerCaseOffset),
 
         G: new Sprite([
             [w, w, w],
             [w, n, n],
             [w, n, n]
-        ]).invertY().rotateRight(),
+        ]).invertY().rotateRight().setPermanentOffset(lowerCaseOffset),
         H: new Sprite([
             [w, w, w],
             [w, n, n],
             [w, w, w]
-        ]).invertY().rotateRight(),
+        ]).invertY().rotateRight().setPermanentOffset(lowerCaseOffset),
         I: new Sprite([
             [w, n, n],
             [w, n, n],
             [w, w, w]
-        ]).invertY().rotateRight(),
+        ]).invertY().rotateRight().setPermanentOffset(lowerCaseOffset),
 
 
         J: new Sprite([
